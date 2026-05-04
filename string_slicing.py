ef3cdcfe4586cad55ccf8c5b68993535276ee096
Manya Tiwari <manyatiwari1904@gmail.com>
2026-05-04 22:37:01 +0530

    Add string_slicing.py, string stricing and its methods

---
+++ string_slicing.py
@@ -0,0 +1,20 @@
+names = "harry,shubman"
+print(names[0:5])
+print(len(names))
+print(names[0:-3])
+nm = "harry"
+print(nm[-4:-2])
+
+print(names.upper())
+print(names.lower())
+print(names.replace("harry","jhonny"))
+print(names.split(" "))
+blogHeading = "introduction to js"
+print(blogHeading.capitalize())
+
+str1 = "he's a happy man and is living peacefully "
+print(len(str1))
+print(len(str1.center(50)))
+print(str1.endswith("e"))
+print(str1.find("is"))
+print(str1.index("is"))
